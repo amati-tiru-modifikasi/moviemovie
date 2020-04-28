8460c15dc849e5fda981e2e429cf4fc43614fe3d
mdestafadilah <desta.08b@gmail.com>
2020-04-28 09:30:05 +0700

    Boilerpart Ready .. Tiru #5cc3a4d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
+{
+  /* https://github.com/reduxjs/redux-thunk/issues/35#issuecomment-184903773 */
+}
 
-render(<App />, document.getElementById('root'));
+import App from "./components/App";
+import reducers from "./reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
