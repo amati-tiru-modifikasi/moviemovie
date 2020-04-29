f0bb3c6f9cdf9965ae27063e2abb2df2530bc006
mdestafadilah <desta.08b@gmail.com>
2020-04-29 13:57:46 +0700

    Genre Fetching ... TIRU #3d19ec4

--- src/components/movielist/Category.js
+++ src/components/movielist/Category.js
@@ -1,17 +1,47 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { setSelectedMenu } from "../../actions";
+import { setSelectedMenu, getMoviesGenre } from "../../actions";
 import NotFound from "../NotFound";
 import styled from "styled-components";
 
-const Category = ({ state, match, setSelectedMenu }) => {
+const MovieWrapper = styled.div`
+  padding: 2rem;
+`;
+
+const MovieImg = styled.img`
+  width: 200px;
+  height: auto;
+`;
+
+const Category = ({
+  genres,
+  match,
+  setSelectedMenu,
+  getMovieGenre,
+  movies,
+  base
+}) => {
   useSetSelected(match.params.name, setSelectedMenu);
-  return <div>{match.params.name}</div>;
+  useFetchMoviesGenre(match.params.name, getMoviesGenre, genres);
+
+  if (!movies.results) {
+    return <div>Loading</div>;
+  }
+
+  console.log(movies.results);
+
+  const baseUrl = base.image.base_url;
+  return <div>{renderMovies(movie.results, baseUrl)}</div>;
 };
 
-const mapStateToProps = state => {
-  return { state };
-};
+function renderMovies(movies, baseUrl) {
+  return movies.map(movie => (
+    <MovieWrapper key={movie.id}>
+      {movie.original_title}
+      <MovieImg src={`${baseUrl}w780${movie.poster_path}`} />
+    </MovieWrapper>
+  ));
+}
 
 function useSetSelected(name, cb) {
   useEffect(() => {
@@ -19,4 +49,16 @@ function useSetSelected(name, cb) {
   }, [name]);
 }
 
-export default connect(mapStateToProps, { setSelectedMenu })(Category);
+function useFetchMoviesGenre(name, cb, genres) {
+  useEffect(() => {
+    cb(name);
+  }, [genres, name]);
+}
+
+const mapStateToProps = ({ geral, movies }) => {
+  return { genres: geral.genres, movies, base: geral.base };
+};
+
+export default connect(mapStateToProps, { setSelectedMenu, getMoviesGenre })(
+  Category
+);
